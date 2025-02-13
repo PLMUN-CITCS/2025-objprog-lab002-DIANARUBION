899850c238bdd5e25f041382dffb26f83f34766c
diana <dianarubion_bsit@plmun.edu.ph>
2025-02-13 22:33:19 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 02

---
+++ InputOutputDemo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class InputOutputDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int userInt = scanner.nextInt();
+
+        System.out.print("Enter a decimal number: ");
+        double userDouble = scanner.nextDouble();
+        scanner.nextLine();  // Consume newline left-over
+
+        System.out.print("Enter a word or sentence: ");
+        String userString = scanner.nextLine();
+
+        System.out.println("\nYour integer: " + userInt);
+        System.out.printf("Your decimal number: %.2f\n", userDouble);
+        System.out.println("Your string: " + userString);
+
+        scanner.close();
+    }
+}
